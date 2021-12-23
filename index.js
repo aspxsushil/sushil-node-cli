ca7ed5338c0a24b674e7eed71f8d3879ded5fe1b
Sushil <sushil_ghimire@homedepot.com>
2021-12-23 12:58:46 -0600

    added chalk implementation

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
+
+const chalk = require('chalk');
 console.clear();
-console.log(`Sushil Ghimire
+const log = console.log;
+log(`${chalk.red.bold('SUSHIL GHIMIRE')}
 A Software Engineer based in houston.
 `);
 
 const packageDetails = require('./package.json');
-console.log(`
-NAME : ${packageDetails.name}
-VERSION: ${packageDetails.version}
+// import { packageDetails } from './package.json';
+
+
+log(`
+${chalk.hex(`#1DA1F2`).bold.inverse('NAME')} : ${chalk.dim(packageDetails.name)}
+${chalk.hex(`#6cc644`).bold.inverse(`VERSION`)}: ${chalk.dim(packageDetails.version)}
+${chalk.hex(`#6937FF`).bold.inverse(`REPOSITORY`)}: ${chalk.dim(packageDetails.repository)}
 
 `)
