495f1bd1d6a7b2c20fad9f0bb958403afab6a737
Sushil <sushil_ghimire@homedepot.com>
2021-12-22 20:38:35 -0600

    added versions

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.clear();
 console.log(`Sushil Ghimire
 A Software Engineer based in houston.
 `);
+
+const packageDetails = require('./package.json');
+console.log(`
+NAME : ${packageDetails.name}
+VERSION: ${packageDetails.version}
+
+`)
